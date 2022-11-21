10b959feef27bc249e9f144099da1c96ed177c9b
SaltysKing <93691524+SaltysKing@users.noreply.github.com>
2022-11-21 11:31:08 -0800

    added console.logs for contribution begging.

--- optional-caution/js-injector.js
+++ optional-caution/js-injector.js
@@ -1,3 +1,4 @@
 
 console.log("BE CAREFUL INJECTING 3rd PARTY JS")
 console.log("This website is using XernSS")
+console.log("if you can see this you might consider contributing @ https://github.com/saltysking/XernSS")
